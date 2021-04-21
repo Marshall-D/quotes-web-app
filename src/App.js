2d1d930c697727f89c4cd4899f8d1cb559dec68e
D1 <d1megaenterprise@gmail.com>
2021-04-21 19:08:24 +0100

    added routing for different pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import {Switch,Route,Redirect} from 'react-router-dom';
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+import AllQuotes from "./pages/AllQuotes";
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
-    <div>
+      <Layout>
+          <Switch>
+              <Route path={'/'} exact>
+                  <Redirect to={'/quotes'}/>
+              </Route>
 
-    </div>
+              <Route path={'/quotes'} exact>
+
+                  <AllQuotes/>
+              </Route>
+
+              <Route path={'/quotes/:quotesId'}>
+
+                  <QuoteDetail/>
+              </Route>
+
+              <Route path={'/new-quote'}>
+                  <NewQuote/>
+
+              </Route>
+
+          </Switch>
+
+      </Layout>
+
   );
 }
 
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,20 @@
+import {useParams,Route} from 'react-router-dom';
+import {Fragment} from "react";
+import Comments from "../components/comments/Comments";
+
+
+const QuoteDetail = () => {
+    const params = useParams()
+
+    return <Fragment>
+        <h1> Quote Detail  Page </h1>
+        <p>{params.quotesId}</p>
+        <Route path={`/quotes/${params.quotesId}/comments`}>
+            <Comments/>
+
+        </Route>
+
+
+    </Fragment>
+};
+export default QuoteDetail;
