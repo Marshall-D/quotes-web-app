720c349a6ce571e2fde928f605371aaaf521b60a
D1 <d1megaenterprise@gmail.com>
2021-04-22 06:20:50 +0100

    taking the user back to the quotes page after submitting a new quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import {useHistory} from 'react-router-dom';
 
 const NewQuote = () => {
+    const history = useHistory();
     const quoteHandler = (userData) => {
         console.log(userData);
+
+        history.push('/quotes');
+
     };
     return <QuoteForm onAddQuote={quoteHandler} >  </QuoteForm>;
 
